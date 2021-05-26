91a84a37072a64fcff347c13e2c701cdf9953b0b
Irza Muhammad Fatir <0320200012@polman.astra.ac.id>
2021-05-26 21:11:17 +0700

    MASUK PINK MASUKKKK

--- BekerjaDenganData2/DeleteKaryawan.Designer.cs
+++ BekerjaDenganData2/DeleteKaryawan.Designer.cs
@@ -42,6 +42,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txtJabatan = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -180,11 +181,22 @@
             this.label7.TabIndex = 60;
             this.label7.Text = "HALOOOO MASUK GA";
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(87, 70);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(249, 24);
+            this.label8.TabIndex = 61;
+            this.label8.Text = "MASUK PINK MASUKKKK";
+            // 
             // DeleteKaryawan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(392, 290);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.txtJabatan);
             this.Controls.Add(this.btnCari);
@@ -222,5 +234,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtJabatan;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
